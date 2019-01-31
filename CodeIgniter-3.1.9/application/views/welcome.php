3cc2a92499e8141a8ad178086ac3e0bdfebff26c
kumarom1203 <kumarom1203>
2019-01-31 13:28:13 +0530

    CodeIgniter-3.1.9 READY TO USE WITH AUTHENTICATION

---
+++ CodeIgniter-3.1.9/application/views/welcome.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Codeigniter-3.1.9</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+<h1 style="text-align: center;">Welcome</h1>
+<a href="<?php echo base_url('Login/login');?>" class="btn btn-primary"> Login</a> 
+<a href="<?php echo base_url('Login/signup');?>" class="btn btn-danger"> Signup</a>
+</div>
+
+</body>
+</html>
